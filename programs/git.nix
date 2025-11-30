0c8bf21d3a86516a16ef4a1a6ba84b4b99999280
RC-14 <61058098+RC-14@users.noreply.github.com>
2025-12-01 00:29:44 +0100

    Update lockfile and adjust git config as required

--- programs/git.nix
+++ programs/git.nix
@@ -1,20 +1,21 @@
 { user, ... }: {
   programs.git = {
     enable = true;
-    userName = user.name;
-    userEmail = user.email;
     ignores = [ ".DS_Store" ];
     lfs.enable = true;
-    extraConfig = {
+    settings = {
       init.defaultBranch = "main";
       protocol.file.allow = true;
       pull.rebase = true;
-    };
-    aliases = {
-      # Undo the last commit (doesn't undo changes)
-      shit = "reset --soft HEAD~1";
-      # Resets tracked files to the state of the latest commit
-      wipe = "reset --hard";
+      user = {
+        inherit (user) name email;
+      };
+      alias = {
+        # Undo the last commit (doesn't undo changes)
+        shit = "reset --soft HEAD~1";
+        # Resets tracked files to the state of the latest commit
+        wipe = "reset --hard";
+      };
     };
   };
 }
